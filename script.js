2b90e16b705d7b6aa8496d184447f5cee2450c5c
Ian E Von Fange <wolfgangvonfange@gmail.com>
2022-10-02 11:46:04 -0500

    Refactored generate password

--- script.js
+++ script.js
@@ -9,57 +9,132 @@ function shuffleArray(array) {
   }
 }
 function generatePassword() {
-// Character possibilites
-  const lettersArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  // Function for building our character array based on user choices. Spread operators used on the push methods to ensure array iterability.
+  function buildArr(prompt, arr) {
+    if (prompt) {
+      charOptions.push(...arr);
+    }
+  }
+
+  function checkPassword(password, array) {
+    // Check for at least one character from each selected character type
+    for (let i = 0; i < array.length; i++) {
+      if (password.includes(array[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Character possibilites
+  const lettersArr = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
   const upperLetters = lettersArr.map((letter) => letter.toUpperCase());
   const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  const specChar = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '/', '<', '=', '>', '?', '@', '[', ']', '^', '{', '|', '}', '~'];
+  const specChar = [
+    '!',
+    '#',
+    '$',
+    '%',
+    '&',
+    '(',
+    ')',
+    '*',
+    '+',
+    '-',
+    '/',
+    '<',
+    '=',
+    '>',
+    '?',
+    '@',
+    '[',
+    ']',
+    '^',
+    '{',
+    '|',
+    '}',
+    '~',
+  ];
   const charOptions = [];
 
   // Get the user-preferred password length (8-128 characters)
-  const pwLength = parseInt(prompt('Passwords must be between 8 and 128 characters. How many characters would you like?'));
-  if (pwLength < 8 || pwLength > 128 || pwLength === null || isNaN(pwLength) === true) {
-    alert('No password will be generated! Please enter a number of characters (between 8 and 128) for your password.');
+  const pwLength = parseInt(
+    prompt(
+      'Passwords must be between 8 and 128 characters. How many characters would you like?'
+    )
+  );
+  if (pwLength < 8 || pwLength > 128 || pwLength === null || isNaN(pwLength)) {
+    alert(
+      'No password will be generated! Please enter a number of characters (between 8 and 128) for your password.'
+    );
   } else {
     alert(`Your password will be ${pwLength} characters long.`);
     // Confirm what characters the user would like to use
-    const lowerCase = confirm('Are lowercase letters ok?');
-    const upperCase = confirm('Are uppercase letters ok?');
-    const numChoice = confirm('Are numbers ok?');
-    const specCharChoice = confirm('Are special characters ok?');
-    // Function for building our character array based on user choices. Rest parameters used on the push methods to ensure iterability.
-    function buildArr(prompt, arr) {
-      if (prompt === true) {
-        charOptions.push(...arr);
-        while (charOptions.length < pwLength) {
-          charOptions.push(...arr);
-        }
-      }
-    }
-    // Build our character array
-    buildArr(lowerCase, lettersArr);
-    buildArr(upperCase, upperLetters);
-    buildArr(numChoice, numbers);
-    buildArr(specCharChoice, specChar);
+    const lowerCase = confirm('Would you like lowercase letters?');
+    const upperCase = confirm('Would you like uppercase letters?');
+    const numChoice = confirm('Would you like numbers?');
+    const specCharChoice = confirm('Would you like special characters?');
+
     // Account for all false case
-    if (lowerCase === false && upperCase === false && numChoice === false && specCharChoice === false) {
-      alert('No password will be generated! Please select at least one character type.');
+    if (!lowerCase && !upperCase && !numChoice && !specCharChoice) {
+      alert(
+        'No password will be generated! Please select at least one character type.'
+      );
+    } else {
+      // Build our character array
+      buildArr(lowerCase, lettersArr);
+      buildArr(upperCase, upperLetters);
+      buildArr(numChoice, numbers);
+      buildArr(specCharChoice, specChar);
     }
-  
-  // console.log(`${charOptions}---unshuffled`);
-  // then call our shuffle function to randomize the array
-  shuffleArray(charOptions);
-  // console.log("---------------------");
-  // console.log(charOptions + " ---shuffled");
-  // Slice our shuffled array to get our final password as an array
-  const pwArray = charOptions.slice(0, pwLength);
-  // And join to create a string version of the final password
-  return pwArray.join('');
- } 
+
+    // console.log(`${charOptions}---unshuffled`);
+    // then call our shuffle function to randomize the array
+    shuffleArray(charOptions);
+    // console.log("---------------------");
+    // console.log(charOptions + " ---shuffled");
+    // Slice our shuffled array to get our final password as an array
+
+    const pwArray = charOptions.slice(0, pwLength);
+    // And join to create a string version of the final password
+    let finalPassword = pwArray.join('');
+    if (!checkPassword(finalPassword, charOptions)) {
+      finalPassword = generatePassword();
+    } else {
+      return finalPassword;
+    }
+  }
 }
 
 function writePassword() {
-  alert("Welcome! I'm going to help you generate a random password today.")
+  alert("Welcome! I'm going to help you generate a random password today.");
   // Finally, push our password string to the document's HTML
   const password = generatePassword();
   const passwordText = document.querySelector('#password');
